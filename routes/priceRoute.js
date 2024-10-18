f5c69feae7ec1229e2c55bdaa53636a5b63506b6
sehabur <sehabur@gmail.com>
2024-10-19 01:06:42 +0600

    ltp to close change; remove unnecessary codes

--- routes/priceRoute.js
+++ routes/priceRoute.js
@@ -1,7 +1,5 @@
 const express = require("express");
 
-const axios = require("axios");
-
 const {
   getAllStocks,
   getStocksList,
@@ -9,7 +7,7 @@ const {
   getMarketStatus,
   latestPrice,
   indexMover,
-  latestPricesBySearch,
+  // latestPricesBySearch,
   sectorGainValueSummary,
   sectorLatestPrice,
   dailySectorPrice,
@@ -25,12 +23,10 @@ const {
   screener,
   topFinancials,
   getIndexInfo,
-  pytest,
   getSymbolTvchart,
   getBarsTvchart,
   marketDepth,
   marketDepthAllInst,
-  newtest,
 } = require("../controllers/priceController");
 
 const router = express.Router();
@@ -51,7 +47,7 @@ router.route("/getMarketStatus").get(getMarketStatus);
 
 router.route("/ipo").get(getIpoList);
 
-router.route("/latestPricesBySearch").get(latestPricesBySearch);
+// router.route("/latestPricesBySearch").get(latestPricesBySearch);
 
 // sector //
 router.route("/sectorGainValueSummary").get(sectorGainValueSummary);
@@ -87,35 +83,9 @@ router.route("/marketDepth").get(marketDepth);
 router.route("/marketDepthAllInst").get(marketDepthAllInst);
 
 // Test functions //
-router.route("/pytest").get(pytest);
-router.route("/newtest").get(newtest);
-router.route("/test").post(async (req, res) => {
-  const data = await axios.request({
-    method: "post",
-    url: "https://www.dsebd.org/ajax/load-instrument.php",
-    headers: {
-      "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8:",
-      Host: "www.dsebd.org",
-      "X-Requested-With": "XMLHttpRequest",
-    },
-    data: {
-      inst: "MALEKSPIN",
-    },
-  });
-  console.log(data.data);
-  res.send(data.data);
-});
-
-// test route for app
-router.route("/user1").get((req, res) => {
-  console.log("user");
-  const data = "User data 22";
-  res.json({ data });
-});
-
-router.route("/product1").get((req, res) => {
-  console.log("product");
-  const data = "Product data 28";
+router.route("/test").get((req, res) => {
+  console.log("Test");
+  const data = "Test data 101";
   res.json({ data });
 });
 
